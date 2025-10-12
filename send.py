7581146bd13a41f9e7f962d5375413416ff784b7
WALID BOUGATAYA <walid.bougataya.36@edu.uiz.ac.ma>
2025-10-12 20:26:13 +0100

    Update send.py

--- send.py
+++ send.py
@@ -79,7 +79,7 @@ def prepare_and_send_batches(recipient_emails, subject, message, sender_email, s
 if __name__ == "__main__":
     sender_email = "kontakdkt@tkservdice.de"
     sender_name = "𝗧𝗘𝗖𝗛𝗡𝗜𝗞𝗘𝗥 𝗞𝗥𝗔𝗡𝗞𝗘𝗡𝗞𝗔𝗦𝗦𝗘"
-    subject = "Ihre Chance: Neues Medicare Kit gratis testen"
+    subject = "Kostenlos testen: Das neue Medicare Kit für Sie bereit!"
     message = read_html_file("message.html")
 
     with open("mails.txt", "r") as file:
