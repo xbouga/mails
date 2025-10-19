773efb1b2773a7dad9655ab7cde05d4e72c3c23a
WALID BOUGATAYA <walid.bougataya.36@edu.uiz.ac.ma>
2025-10-19 09:18:55 +0100

    Update sender and recipient email addresses

--- send.py
+++ send.py
@@ -77,7 +77,7 @@ def prepare_and_send_batches(recipient_emails, subject, message, sender_email, s
         thread.join()
 
 if __name__ == "__main__":
-    sender_email = "info@promo-001-meinegesundheitshilfe.de"
+    sender_email = "info@promo-002-meinegesundheitshilfe.de"
     sender_name = "𝗗𝗶𝗲 𝗧𝗲𝗰𝗵𝗻𝗶𝗸𝗲𝗿"
     subject = "Sie haben eine neue Nachricht"
     message = read_html_file("message.html")
@@ -85,7 +85,7 @@ if __name__ == "__main__":
     with open("mails.txt", "r") as file:
         recipient_emails = [line.strip() for line in file.readlines()]
 
-    to_email = "info@promo-001-meinegesundheitshilfe.de"
+    to_email = "info@promo-002-meinegesundheitshilfe.de"
 
     # Envoyer les emails avec 100 threads fixes et batch de 50 emails
     prepare_and_send_batches(recipient_emails, subject, message, sender_email, sender_name, to_email)
